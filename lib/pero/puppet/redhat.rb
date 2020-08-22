eb8ddc8499add76c79298d3ddb80b0cd210b2e73
pyama86 <pyama@pepabo.com>
2020-08-23 01:36:49 +0900

    always reinstall release package

--- lib/pero/puppet/redhat.rb
+++ lib/pero/puppet/redhat.rb
@@ -14,16 +14,16 @@ module Pero
           ["puppet6-release-el-#{main_release}.noarch.rpm", "puppet-agent"]
         end
 
-        installed = run_specinfra(:check_package_is_installed, release_package.gsub(/-el.*/, ''))
-        unless installed
-
-          Pero.log.info "install package #{release_package}"
-          run_specinfra(:remove_package, "puppet5-release")
-          run_specinfra(:remove_package, "puppet6-release")
-          raise "failed package install:#{release_package}" if specinfra.run_command("rpm -ivh https://yum.puppetlabs.com/#{release_package}").exit_status != 0
-        end
-
         unless run_specinfra(:check_package_is_installed, package_name, version)
+          installed = run_specinfra(:check_package_is_installed, release_package.gsub(/-el.*/, ''))
+          unless installed
+            Pero.log.info "install package #{release_package}"
+            run_specinfra(:remove_package, "puppetlabs-release")
+            run_specinfra(:remove_package, "puppet5-release")
+            run_specinfra(:remove_package, "puppet6-release")
+            raise "failed package install:#{release_package}" if specinfra.run_command("rpm -ivh https://yum.puppetlabs.com/#{release_package}").exit_status != 0
+          end
+
           Pero.log.info "install package #{package_name}-#{version}"
           raise "failed package uninstall:#{package_name}" if run_specinfra(:remove_package, package_name).exit_status != 0
           raise "failed package install:#{package_name} version #{version}" if run_specinfra(:install_package, package_name, version).exit_status != 0
