860d57ee1042d970e7da412a0c9e9d3e4de8c14b
pyama86 <pyama@pepabo.com>
2020-08-25 11:37:46 +0900

    if

--- lib/pero/puppet.rb
+++ lib/pero/puppet.rb
@@ -123,6 +123,7 @@ module Pero
                              Pero.log.info "#{host}:#{data.chomp}" if stream == :stdout && data.chomp != ""
                              Pero.log.warn "#{host}:#{data.chomp}" if stream == :stderr && data.chomp != ""
                            end
+            ssh.exec!(specinfra.build_command("rm -rf /tmp/puppet/#{tmpdir}")) if @options["one-shot"]
             ssh.loop {true} if ENV['PERO_DEBUG']
           end
         rescue => e
