a2750fb4b297877288247313ec59d68c45311803
k1LoW <k1lowxb@gmail.com>
2021-04-08 23:58:16 +0900

    Add a workaround to make `puppet master` successful.

--- lib/pero/docker.rb
+++ lib/pero/docker.rb
@@ -178,7 +178,12 @@ RUN echo -e "#{puppet_config.split(/\n/).join("\\n")}" > #{conf_dir}/puppet.conf
     end
 
     def run_cmd
-      release_package,package_name, conf_dir  = if Gem::Version.new("5.0.0") > Gem::Version.new(server_version)
+      if Gem::Version.new("3.0.0") > Gem::Version.new(server_version)
+        # /var/puppet/run is created for the first time by running `puppet master`,
+        # but `puppet master` will fail because the permissions are wrong.
+        # So, let the `puppet master` fail once, fix the permission of /var/puppet/run, and execute `puppet master` again.
+        'puppet master --no-daemonize --verbose || (chown puppet: /var/puppet/run && puppet master --no-daemonize --verbose)'
+      elsif Gem::Version.new("5.0.0") > Gem::Version.new(server_version)
         'puppet master --no-daemonize --verbose'
       elsif Gem::Version.new("6.0.0") > Gem::Version.new(server_version)
         'puppetserver foreground'
